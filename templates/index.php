48a57279ab91153929ff3068cfea47d3f0eff6bc
ecole-machette <104460833+ecole-machette@users.noreply.github.com>
2022-12-16 19:04:38 +0000

    ffd

--- templates/index.php
+++ templates/index.php
@@ -9,12 +9,12 @@ $js = file_get_contents('script.js');
 $nb_green = mb_substr_count($css, "green");
 echo "<div style='visibility: hidden;'>";
 
-if ($nb_green == 1) {
+if ($nb_green >= 1) {
     echo("TECHIO> success true \r\n");
     echo("TECHIO> message --channel Super! Tu as trouvé la bonne solution. \r\n");
 } else {
     echo("TECHIO> success false  \r\n");
-	echo("TECHIO> message --channel Dommage ! Le style, ça se passe dans la feuille de style ! Es-tu aller voir là-bas ? \r\n");
+	echo("TECHIO> message --channel Dommage&nbsp! Le style, ça se passe dans la feuille de style ! Es-tu allé voir là-bas ? \r\n");
 }
 
 
