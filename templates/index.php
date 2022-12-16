3f5f4f9095c7a69f45f7079ca954cf91a0dbc311
ecole-machette <104460833+ecole-machette@users.noreply.github.com>
2022-12-16 18:14:52 +0000

    g

--- templates/index.php
+++ templates/index.php
@@ -6,6 +6,7 @@ $var = file_get_contents('index.html');
 
 // echo $var;
 echo "TECHIO> open -s /project/target/data/public_html";
+
 echo("TECHIO> success true \r\n");
 echo("TECHIO> message --channel Super! Une solution facilement adaptable. \r\n");
 
