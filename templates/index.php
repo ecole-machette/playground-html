b2ef0dea801da39def9aa0e02ad00abd28b1e6ae
ecole-machette <104460833+ecole-machette@users.noreply.github.com>
2022-12-16 18:02:09 +0000

    ghj

--- templates/index.php
+++ templates/index.php
@@ -4,8 +4,8 @@ include('index.html');
 
 $var = file_get_contents('index.html');
 
-echo $var;
-
+// echo $var;
+echo "TECHIO> open -s /project/target/data/public_html";
 echo("TECHIO> success true \r\n");
 echo("TECHIO> message --channel Super! Une solution facilement adaptable. \r\n");
 
